3730adf1d3fb65a89d153491a8f3214374ed5634
Anthoni Broering dos Santos <anthoniibs@gmail.com>
2022-12-11 13:12:41 -0300

    feat:  ✨  adicionado botão de troca de tema

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,8 +2,57 @@ import { HeaderMain, Navigating } from "./styles";
 
 import logo from "../../assets/svg/logo-nerdices-geek.svg";
 import { NavLink } from "react-router-dom";
+import { useTheme } from "../../hooks/useTheme";
+import { dark, light } from "../../styles/themes";
+
+import { IoIosSunny, IoIosPartlySunny } from "react-icons/io";
+import { BiArrowToTop } from "react-icons/bi";
+
+import { useEffect, useRef, useState } from "react";
 
 const Header = () => {
+
+  const { theme, setTheme } = useTheme();
+  const [visible, setVisible] = useState(false);
+
+  const cbRef = useRef(toggleVisible);
+
+  useEffect(() => {
+    cbRef.current = toggleVisible;
+  });
+
+  useEffect(() => {
+    const cb = () => cbRef.current();
+    window.addEventListener("scroll", cb);
+
+    return () => {
+      window.removeEventListener("scroll", cb);
+    };
+  }, []);
+
+
+  function handleToggleTheme() {
+    setTheme(theme.title === "dark" ? light : dark);
+  }
+
+  function toggleVisible() {
+    const scrolled = document.documentElement.scrollTop;
+    if (scrolled > 300) {
+      setVisible(true);
+    }
+    else if (scrolled <= 300) {
+      setVisible(false);
+    }
+  }
+
+  function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  }
+
+
   return (
     <HeaderMain>
       <img src={logo} alt="Logo Nerdices Geek" />
@@ -50,11 +99,25 @@ const Header = () => {
             </NavLink>
           </li>
         </ul>
+        <button
+          id="btnTheme"
+          onClick={handleToggleTheme}
+        >
+          {theme.title === "dark" ? <IoIosSunny size={24} /> : <IoIosPartlySunny size={24} />}
+        </button>
 
         <button>
           Minha Conta
         </button>
       </Navigating>
+
+      {visible && (
+        <button onClick={scrollToTop}
+          id="arrow-up"
+        >
+          <BiArrowToTop size={28} />
+        </button>
+      )}
     </HeaderMain >
   );
 };
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,34 +1,61 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const animateScrollTop = keyframes`
+  from {
+    opacity: 0;
+    transform: scale(0);
+    display: block;
+  }
+
+  to {
+    opacity: 1;
+    transform: scale(1);
+  }
+`;
 
 export const HeaderMain = styled.header`
-  height: 80px;
-  background: ${props => props.theme.colors.backgroundBody};
-  grid-area: HEADER;
-  display: flex;
   align-items: center;
-  justify-content: space-between;
-  width: 100%;
-  padding: 0 10%;
+  background: ${props => props.theme.colors.backgroundBody};
+  display: flex;
   font-family: 'Inter', sans-serif;
+  grid-area: HEADER;
+  height: 80px;
+  justify-content: space-between;
+  padding: 0 10%;
   position: fixed;
+  transition: all .2s ease;
+  width: 100%;
   z-index: 1000;
 
   img {
     display: block;
     width: 60px;
   }
+
+  #arrow-up {
+    animation: ${animateScrollTop} 1s ease;
+    bottom: 1rem;
+    border-radius: 50%;
+    border: none;
+    background: ${props => props.theme.colors.background};
+    color: ${props => props.theme.colors.text.secondary};
+    height: 50px;
+    position: fixed;
+    right: 1rem;
+    width: 50px;
+  }
 `;
 
 export const Navigating = styled.nav`
   display: flex;
-  gap: 1rem;
   font-size: 1rem;
+  gap: 1rem;
 
   ul {
-    display: flex;
     align-items: center;
-    gap: 3rem;
+    display: flex;
     flex-wrap: wrap;
+    gap: 3rem;
     margin-right: 3rem;
 
     a {
@@ -46,21 +73,36 @@ export const Navigating = styled.nav`
   }
 
   button {
-    white-space: nowrap;
-    font-size: 1rem;
+    color: ${props => props.theme.colors.text.default};
+    border-radius: 3rem;
     background: transparent;
     border: 1px ${props => props.theme.colors.background} solid;
-    color: ${props => props.theme.colors.text.default};
+    font-size: 1rem;
     padding: .3rem 1.6rem;
-    border-radius: 3rem;
     transition: background .3s ease-in;
+    white-space: nowrap;
 
     &:hover {
-      background: ${props => props.theme.colors.background}
+      color: ${props => props.theme.colors.link};
+      background: ${props => props.theme.colors.background};
     }
 
     &:active {
       background: ${props => props.theme.colors.isActive}
     }
   }
+
+  #btnTheme {
+    display: flex;
+    background-color: transparent;
+    border: none;
+    padding: .3rem;
+    color: ${({ theme }) => theme.colors.text.primary};
+    transition: color .2s ease-in;
+
+    &:hover {
+      color: ${({ theme }) => theme.colors.isHover};
+    }
+  }
+
 `;
